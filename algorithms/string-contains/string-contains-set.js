4ec39194eaa79316783fdab72601ba796df4ee1b
Kevin <kviglucci@gmail.com>
2019-06-09 01:15:50 -0500

    update string contains implementation with simplified algorithm

--- algorithms/string-contains/string-contains-set.js
+++ algorithms/string-contains/string-contains-set.js
@@ -1,19 +1,16 @@
 const contains = (subject, search) => {
     const subjectChars = subject.split("");
     const searchLength = search.length;
-    const firstSearchChar = sarch.split("")[0];
-    return subject
-        .split("")
-        .map((subjectChar, i) => {
-            if (subjectChar == firstSearchChar) {
-                const potentialMatchChars = subjectChars.slice(i, i + searchLength);
-                return potentialMatchChars.join("");
+    const firstSearchChar = search.split("")[0];
+    return subjectChars.some((char, i) => {
+        if (char == firstSearchChar) {
+            const potentialMatchChars = subjectChars.slice(i, i + searchLength);
+            const potentialMatchString = potentialMatchChars.join("");
+            if (potentialMatchString === search) {
+                return true;
             }
-        })
-        .filter((potentialMatch) => {
-            return potentialMatch === search;
-        })
-        .length > 0;
+        }
+    });
 };
 
 console.log(contains("quick brown fox", "brown")); // true
