f1d096b878e814f87e1685e14c7164c667169e83
Kevin <kviglucci@gmail.com>
2019-06-09 01:32:34 -0500

    refactor contains to use for loop instead of array methods

--- algorithms/string-contains/string-contains-set.js
+++ algorithms/string-contains/string-contains-set.js
@@ -2,7 +2,11 @@ const contains = (subject, search) => {
     const subjectChars = subject.split("");
     const searchLength = search.length;
     const firstSearchChar = search.split("")[0];
-    return subjectChars.some((char, i) => {
+    for (var i = 0; i < subjectChars.length; i++) {
+        if (searchLength + i > subjectChars.length) {
+            return false;
+        }
+        const char = subjectChars[i];
         if (char == firstSearchChar) {
             const potentialMatchChars = subjectChars.slice(i, i + searchLength);
             const potentialMatchString = potentialMatchChars.join("");
@@ -10,7 +14,7 @@ const contains = (subject, search) => {
                 return true;
             }
         }
-    });
+    }
 };
 
 console.log(contains("quick brown fox", "brown")); // true
