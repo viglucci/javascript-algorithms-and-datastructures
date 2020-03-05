9614779c02f058aa31323e1cbd734050e5a83e37
Kevin Viglucci <kviglucci@gmail.com>
2020-03-05 08:51:13 -0600

    refactor string contains impl

--- algorithms/string-contains/string-contains-set.js
+++ algorithms/string-contains/string-contains-set.js
@@ -1,30 +1,35 @@
-import LinkedList from "../../datastructures/LinkedList/LinkedList";
-import {expect} from "chai";
+import {expect} from 'chai'
 
 const contains = (subject, search) => {
-    const subjectChars = subject.split("");
-    const searchLength = search.length;
-    const firstSearchChar = search.split("")[0];
-    for (var i = 0; i < subjectChars.length; i++) {
-        if (searchLength + i > subjectChars.length) {
-            return false;
-        }
-        const char = subjectChars[i];
-        if (char === firstSearchChar) {
-            const potentialMatchChars = subjectChars.slice(i, i + searchLength);
-            const potentialMatchString = potentialMatchChars.join("");
-            if (potentialMatchString === search) {
+    if(!subject || !search) return false;
+    if(search.length > subject.length) return false;
+
+    let matches = 0;
+    for (let i = 0; i < subject.length; i++) {
+        let subjectCursor = i;
+        for (let j = 0; j < search.length; j++) {
+            if (subject.charAt(subjectCursor++) === search.charAt(j)) {
+                matches += 1;
+            } else {
+                matches = 0;
+            }
+            if (matches === search.length) {
                 return true;
             }
         }
     }
+
+    return false;
 };
 
 describe('contains', () => {
     it('returns true or false depending if the string contains the other string', () => {
-        expect(contains("quick brown fox", "brown")).to.equal(true);
-        expect(contains("br o w n", "brown")).to.equal(false);
-        expect(contains("andre", "kevin")).to.equal(false);
-        expect(contains("aaabcd", "aabc")).to.equal(true);
+        expect(contains('', 'brown')).to.equal(false);
+        expect(contains('quick brown fox', '')).to.equal(false);
+        expect(contains('quick brown', 'quick brown fox')).to.equal(false);
+        expect(contains('quick brown fox', 'brown')).to.equal(true);
+        expect(contains('br o w n', 'brown')).to.equal(false);
+        expect(contains('andre', 'kevin')).to.equal(false);
+        expect(contains('aaabcd', 'aabc')).to.equal(true);
     });
 });
